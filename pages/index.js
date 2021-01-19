628c84718120fe398ff79b37b791e3817ed825a0
ggyu <kcpooh0202@naver.com>
2021-01-19 11:17:56 +0900

    react-nodebird

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"; // next.js 에서 생략 가능(상관없음)
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
